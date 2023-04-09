10838b6543c5fcbd521665ed2f2c3a226bda4b1b
ShantanuDas99 <procodeshanti@gmail.com>
2023-04-09 09:25:21 +0600

    initial data load

---
+++ src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h1>Thanks for the review</h1>
+        </div>
+    );
+};
+
+export default OrderReview;
